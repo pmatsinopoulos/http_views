6989eb645b527e186f6998536bf7d212bbce227b
Panayotis Matsinopoulos <>
2018-07-25 22:44:35 +0300

    Main start

---
+++ main/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def current_datetime(request):
+    now = datetime.now()
+    html = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Current DateTime</title>
+        </head>
+        <body>
+            <h1>{}</h1>
+        </body>
+    </html>
+    """.format(now)
+    return HttpResponse(html, status=200)
+
